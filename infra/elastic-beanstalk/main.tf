b9ac304cba4f0fcf396e2255066e0adaabd9688a
JefersonSchneider <jefesch@gmail.com>
2025-04-23 16:53:48 -0300

    ajuste deploy
    ajuste deploy

--- infra/elastic-beanstalk/main.tf
+++ infra/elastic-beanstalk/main.tf
@@ -4,17 +4,47 @@ provider "aws" {
 
 variable "app_name" {
   type    = string
-  default = "knowledge-backend"
+  default = "knowledge-backend2"
 }
 
 variable "env_name" {
   type    = string
-  default = "knowledge-backend-env"
+  default = "knowledge-backend2-env"
 }
 
 variable "mongodb_uri" {
   type      = string
   sensitive = true
+}
+
+# Cria a role IAM para o Elastic Beanstalk
+resource "aws_iam_role" "beanstalk_instance_role" {
+  name = "aws-elasticbeanstalk-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Anexa a política gerenciada padrão do Elastic Beanstalk à role
+resource "aws_iam_role_policy_attachment" "beanstalk_instance_policy" {
+  role       = aws_iam_role.beanstalk_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+}
+
+# Cria o instance profile associado à role
+resource "aws_iam_instance_profile" "beanstalk_instance_profile" {
+  name = "aws-elasticbeanstalk-ec2-profile"
+  role = aws_iam_role.beanstalk_instance_role.name
 }
 
 # Cria a aplicação no Elastic Beanstalk
@@ -28,30 +58,46 @@ resource "aws_elastic_beanstalk_environment" "backend_env" {
   name                = var.env_name
   application         = aws_elastic_beanstalk_application.backend_app.name
   solution_stack_name = "64bit Amazon Linux 2023 v6.5.1 running Node.js 22"
+  wait_for_ready_timeout = "30m"
 
-  # Configurações do ambiente
+  # Associa o instance profile ao ambiente
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.beanstalk_instance_profile.name
+  }
+
+  # Configura como single instance (sem load balancer)
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "SingleInstance"
+  }
+
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "InstanceType"
     value     = "t3.micro"
   }
 
- 
-  # Configurações de variável de ambiente (para conectar ao MongoDB Atlas)
+  setting {
+    namespace = "aws:elasticbeanstalk:container:nodejs"
+    name      = "NodeVersion"
+    value     = "22.2.0"
+  }
+
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name      = "MONGODB_URI"
     value     = var.mongodb_uri
   }
 
-  # Configurações de porta
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name      = "PORT"
     value     = "3000"
   }
 
-  # Configurações de escalabilidade mínima
   setting {
     namespace = "aws:autoscaling:asg"
     name      = "MinSize"
@@ -61,10 +107,9 @@ resource "aws_elastic_beanstalk_environment" "backend_env" {
   setting {
     namespace = "aws:autoscaling:asg"
     name      = "MaxSize"
-    value     = "2"
+    value     = "1"
   }
 
-  # Habilitar logs no CloudWatch
   setting {
     namespace = "aws:elasticbeanstalk:cloudwatch:logs"
     name      = "StreamLogs"
@@ -72,7 +117,7 @@ resource "aws_elastic_beanstalk_environment" "backend_env" {
   }
 }
 
-# Output da URL do ambiente.
+# Output da URL do ambiente
 output "environment_url" {
   value       = aws_elastic_beanstalk_environment.backend_env.endpoint_url
   description = "URL do ambiente Elastic Beanstalk"

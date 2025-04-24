dbb5e29a8d6bfbcf4ed835c0980b8a656fb2bd32
JefersonSchneider <jefesch@gmail.com>
2025-04-24 15:15:50 -0300

    ajuste-admin
    ajuste-admin

--- versao-final/backend/config/admin.js
+++ versao-final/backend/config/admin.js
@@ -1,3 +1,16 @@
+module.exports = (req, res, next) => {
+    // Se não houver autenticação, permite acesso (para testes)
+    if (!req.user) {
+        console.log('Nenhum usuário autenticado. Permitindo acesso temporariamente.')
+        return next()
+    }
+
+    if (!req.user.admin) {
+        return res.status(403).send('Acesso negado. Usuário não é administrador.')
+    }
+    next()
+}
+/*
 module.exports = middleware => {
     return (req, res, next) => {
         if(req.user.admin) {
@@ -7,3 +20,4 @@ module.exports = middleware => {
         }
     }
 }
+    */
--- versao-final/backend/config/routes.js
+++ versao-final/backend/config/routes.js
@@ -8,20 +8,24 @@ module.exports = app => {
 
   app.route('/users')
     // .all(app.config.passport.authenticate()) // Remover autenticação
-    .post(admin(app.api.user.save))
-    .get(admin(app.api.user.get));
+    //.post(admin(app.api.user.save))
+    //.get(admin(app.api.user.get));
+    .post(app.api.user.save)
+    .get(app.api.user.get);
 
   app.route('/users/:id')
     // .all(app.config.passport.authenticate()) // Já estava comentado
     .put(app.api.user.save)
     .get(app.api.user.getById)
-    // .get(app.api.user.getById) // Removido o duplicado
+    //.get(app.api.user.getById) // Removido o duplicado
     .delete(app.api.user.remove);
 
   app.route('/categories')
     // .all(app.config.passport.authenticate())
-    .get(admin(app.api.category.get))
-    .post(admin(app.api.category.save));
+    //.get(admin(app.api.category.get))
+    //.post(admin(app.api.category.save));
+    .get(app.api.category.get)
+    .post(app.api.category.save);
 
   // Cuidado com ordem! Tem que vir antes de /categories/:id
   app.route('/categories/tree')
@@ -31,19 +35,25 @@ module.exports = app => {
   app.route('/categories/:id')
     // .all(app.config.passport.authenticate())
     .get(app.api.category.getById)
-    .put(admin(app.api.category.save))
-    .delete(admin(app.api.category.remove));
+    //.put(admin(app.api.category.save))
+    //.delete(admin(app.api.category.remove));
+    .put(app.api.category.save)
+    .delete(app.api.category.remove);
 
   app.route('/articles')
     // .all(app.config.passport.authenticate())
-    .get(admin(app.api.article.get))
-    .post(admin(app.api.article.save));
+    //.get(admin(app.api.article.get))
+    //.post(admin(app.api.article.save));
+    .get(app.api.article.get)
+    .post(app.api.article.save);
 
   app.route('/articles/:id')
     // .all(app.config.passport.authenticate())
     .get(app.api.article.getById)
-    .put(admin(app.api.article.save))
-    .delete(admin(app.api.article.remove));
+    //.put(admin(app.api.article.save))
+    //.delete(admin(app.api.article.remove));
+    .put(app.api.article.save)
+    .delete(app.api.article.remove);
 
   app.route('/categories/:id/articles')
     // .all(app.config.passport.authenticate())

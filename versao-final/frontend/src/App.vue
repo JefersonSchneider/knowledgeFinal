59067a6585e873c5889f81453a382bd214c014f2
JefersonSchneider <jefesch@gmail.com>
2025-04-24 14:59:57 -0300

    ajustes
    ajustes

--- versao-final/frontend/src/App.vue
+++ versao-final/frontend/src/App.vue
@@ -1,13 +1,13 @@
 <template>
-	<div id="app" :class="{'hide-menu': !isMenuVisible || !user}">
-		<Header title="PUC - Base de Conhecimento/ Disciplina: Projetos Integrados" 
-			:hideToggle="!user"
-			:hideUserDropdown="!user" />
-		<Menu v-if="user" />
-		<Loading v-if="validatingToken" />
-		<Content v-else />
-		<Footer />
-	</div>
+    <div id="app" :class="{'hide-menu': !isMenuVisible || !user}">
+        <Header title="PUC - Base de Conhecimento/ Disciplina: Projetos Integrados" 
+            :hideToggle="!user"
+            :hideUserDropdown="!user" />
+        <Menu v-if="user" />
+        <Loading v-if="validatingToken" />
+        <Content v-else />
+        <Footer />
+    </div>
 </template>
 
 <script>
@@ -21,79 +21,104 @@ import Footer from "@/components/template/Footer"
 import Loading from "@/components/template/Loading"
 
 export default {
-	name: "App",
-	components: { Header, Menu, Content, Footer, Loading },
-	computed: mapState(['isMenuVisible', 'user']),
-	data: function() {
-		return {
-			validatingToken: true
-		}
-	},
-	methods: {
-		async validateToken() {
-			this.validatingToken = true
+    name: "App",
+    components: { Header, Menu, Content, Footer, Loading },
+    computed: mapState(['isMenuVisible', 'user']),
+    data: function() {
+        return {
+            validatingToken: true
+        }
+    },
+    methods: {
+        async validateToken() {
+            this.validatingToken = true
 
-			const json = localStorage.getItem(userKey)
-			const userData = JSON.parse(json)
-			this.$store.commit('setUser', null)
+            const json = localStorage.getItem(userKey)
+            const userData = JSON.parse(json)
+            this.$store.commit('setUser', null)
 
-			if(!userData) {
-				this.validatingToken = false
-				this.$router.push({ name: 'auth' })
-				return
-			}
-			console.log('Base URL configurada:', this.$http.defaults.baseURL); // Log da URL base
+            if (!userData) {
+                this.validatingToken = false
+                // Comentado temporariamente para evitar redirecionamento para a página de login
+                // this.$router.push({ name: 'auth' })
+                // Simula um usuário para testes
+                const tempUser = { id: 1, name: 'Teste', email: 'teste@teste.com', admin: true }
+                this.$store.commit('setUser', tempUser)
+                return
+            }
+
+            console.log('Base URL configurada:', this.$http.defaults.baseURL);
             console.log('Enviando requisição para /validateToken com dados:', userData);
-			//const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
-			const res = await this.$http.post(`/validateToken`, userData); // Use this.$http
+            // Comentado temporariamente para evitar a chamada à rota /validateToken
+            /*
+            const res = await this.$http.post(`/validateToken`, userData);
 
-			if (res.data) {
-				this.$store.commit('setUser', userData)
-				
-				if(this.$mq === 'xs' || this.$mq === 'sm') {
-					this.$store.commit('toggleMenu', false)
-				}
-			} else {
-				localStorage.removeItem(userKey)
-				this.$router.push({ name: 'auth' })
-			}
+            if (res.data) {
+                this.$store.commit('setUser', userData)
+                
+                if (this.$mq === 'xs' || this.$mq === 'sm') {
+                    this.$store.commit('toggleMenu', false)
+                }
+            } else {
+                localStorage.removeItem(userKey)
+                this.$router.push({ name: 'auth' })
+            }
+            */
 
-			this.validatingToken = false
-		}
-	},
-	created() {
-		this.validateToken()
-	}
+            // Mantém o usuário do localStorage (se existir) ou usa um temporário
+            this.$store.commit('setUser', userData || { id: 1, name: 'Teste', email: 'teste@teste.com', admin: true })
+
+            if (this.$mq === 'xs' || this.$mq === 'sm') {
+                this.$store.commit('toggleMenu', false)
+            }
+
+            this.validatingToken = false
+        }
+    },
+    created() {
+        // Comentado temporariamente para evitar a validação do token ao iniciar a aplicação
+        // this.validateToken()
+        
+        // Simula um usuário para testes e desativa o estado de "loading"
+        const json = localStorage.getItem(userKey)
+        const userData = JSON.parse(json)
+        this.$store.commit('setUser', userData || { id: 1, name: 'Teste', email: 'teste@teste.com', admin: true })
+        this.validatingToken = false
+
+        if (this.$mq === 'xs' || this.$mq === 'sm') {
+            this.$store.commit('toggleMenu', false)
+        }
+    }
 }
 </script>
 
 <style>
-	* {
-		font-family: "Lato", sans-serif;
-	}
+    * {
+        font-family: "Lato", sans-serif;
+    }
 
-	body {
-		margin: 0;
-	}
+    body {
+        margin: 0;
+    }
 
-	#app {
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
+    #app {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
 
-		height: 100vh;
-		display: grid;
-		grid-template-rows: 60px 1fr 40px;
-		grid-template-columns: 300px 1fr;
-		grid-template-areas:
-			"header header"
-			"menu content"
-			"menu footer";
-	}
+        height: 100vh;
+        display: grid;
+        grid-template-rows: 60px 1fr 40px;
+        grid-template-columns: 300px 1fr;
+        grid-template-areas:
+            "header header"
+            "menu content"
+            "menu footer";
+    }
 
-	#app.hide-menu {
-		grid-template-areas:
-			"header header"
-			"content content"
-			"footer footer";
-	}
+    #app.hide-menu {
+        grid-template-areas:
+            "header header"
+            "content content"
+            "footer footer";
+    }
 </style>
--- versao-final/frontend/src/global.js
+++ versao-final/frontend/src/global.js
@@ -3,7 +3,6 @@ import Vue from 'vue'
 export const userKey = '__knowledge_user'
 //export const baseApiUrl = 'http://localhost:4000'
 export const baseApiUrl = process.env.VUE_APP_API_URL || 'http://knowledge-backend-env.eba-5khzgqpw.us-east-1.elasticbeanstalk.com'
-
 export function showError(e) {
     if(e && e.response && e.response.data) {
         Vue.toasted.global.defaultError({ msg : e.response.data })

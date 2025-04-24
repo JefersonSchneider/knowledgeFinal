42619422a777e5e11046bd60271f2546191b1acf
JefersonSchneider <jefesch@gmail.com>
2025-04-24 09:57:23 -0300

    ajuste knex
    ajuste knex voltando versao

--- versao-final/backend/config/db.js
+++ versao-final/backend/config/db.js
@@ -1,7 +1,8 @@
 const knex = require('knex');
 
 // Carrega a configuração do ambiente 'development'
-const config = require('../knexfile').development;
+const config = require('../knexfile');
+console.log('Configuração do Knex:', config); // Log para depuração
 
 // Inicializa o Knex com a configuração
 const db = knex(config);
